573b1b2ee212da76513d22e8115b8d25e5b4d4bf
Jason Markosky <jasonma@vecima.com>
2018-06-04 16:42:34 -0600

    Convesion to MAZ11254

--- max11254.h
+++ max11254.h
@@ -9,8 +9,8 @@
   */
   
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAX11210_H
-#define __MAX11210_H
+#ifndef __MAX11254_H
+#define __MAX11254_H
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -28,27 +28,25 @@
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
-/*
-#ifdef BOARD_BANGOOD_DEVKIT
-#define csPin   5 // csPin
-#define clkPin  18// CLK
-#define mosiPin 23// MOSI
-#define misoPin 19 // MISO
+
+//#define MAX11210
+#define MAX11254
+
+#ifdef MAX11210
+  #define	MAX11210_SPI_BANK		NotUSED
+  #define	MAX11210_SPI_NSS		csPin
+  #define	MAX11210_SPI_SCK		clkPin
+  #define	MAX11210_SPI_MISO		misoPin
+  #define	MAX11210_SPI_MOSI		mosiPin  
 #endif
 
-#ifdef BOARD_ESPRESSIF_WROVER_DEVKIT
-#define csPin   15 // csPin
-#define clkPin  14// CLK
-#define mosiPin 13// MOSI
-#define misoPin 12 // MISO
+#ifdef MAX11254
+  #define	MAX11254_SPI_BANK		NotUSED
+  #define	MAX11254_SPI_NSS		csPin
+  #define	MAX11254_SPI_SCK		clkPin
+  #define	MAX11254_SPI_MISO		misoPin
+  #define	MAX11254_SPI_MOSI		mosiPin
 #endif
-*/
-
-#define	MAX11210_SPI_BANK		NotUSED
-#define	MAX11210_SPI_NSS		csPin
-#define	MAX11210_SPI_SCK		clkPin
-#define	MAX11210_SPI_MISO		misoPin
-#define	MAX11210_SPI_MOSI		mosiPin
 
 /*
 #define	AD_GPIO1_BANK				NotUSED
@@ -59,38 +57,118 @@
 */
 
 // REGISTER ADDRESS
-#define	STAT1	0x0	// reg size = 8 bits
-#define	CTRL1	0x1	// reg size = 8 bits
-#define	CTRL2	0x2	// reg size = 8 bits (FOR GPIO pins)
-#define	CTRL3	0x3	// reg size = 8 bits
-#define	DATA	0x4	// reg size = 24 bits
-#define	SOC		0x5	// reg size = 24 bits		(System Offset Calibration)
-#define	SGC		0x6	// reg size = 24 bits		(System Gain Calibration)
-#define	SCOC	0x7	// reg size = 24 bits		(Self-calibration Offset)
-#define	SCGC	0x8	// reg size = 24 bits		(Self-calibration Gain)
+#ifdef MAX11210
+  #define	STAT1	0x0	// reg size = 8 bits
+  #define	CTRL1	0x1	// reg size = 8 bits
+  #define	CTRL2	0x2	// reg size = 8 bits (FOR GPIO pins)
+  #define	CTRL3	0x3	// reg size = 8 bits
+  #define	DATA	0x4	// reg size = 24 bits
+  #define	SOC		0x5	// reg size = 24 bits		(System Offset Calibration)
+  #define	SGC		0x6	// reg size = 24 bits		(System Gain Calibration)
+  #define	SCOC	0x7	// reg size = 24 bits		(Self-calibration Offset)
+  #define	SCGC	0x8	// reg size = 24 bits		(Self-calibration Gain)
+#endif
 
-// COMMAND BYTES  (MODE = 0, LINEF = 0)
-#define	SELF_CALIB				0x90
-#define	SYS_OFFSET_CALIB	0xA0
-#define	SYS_GAIN_CALIB		0xB0
-#define	POWER_DOWN				0x88
+#ifdef MAX11254
+    #define	STAT1	    0x0	// reg size = 8 bits
+    #define	CTRL1	    0x1	// reg size = 8 bits
+    #define	CTRL2	    0x2	// reg size = 8 bits (FOR GPIO pins)
+    #define	CTRL3	    0x3	// reg size = 8 bits
+    #define GPIO_CTRL 0x4
+    #define DELAY     0x5
+    #define CHMAP1    0X6
+    #define CHMAP2    0x7
+    #define SEQ       0x8
+    #define GPO_DIR   0x9          
+    #define	SOC		    0x10	// reg size = 24 bits		(System Offset Calibration)
+    #define	SGC		    0x11// reg size = 24 bits		(System Gain Calibration)
+    #define	SCOC	    0x12	// reg size = 24 bits		(Self-calibration Offset)
+    #define	SCGC	    0x13	// reg size = 24 bits		(Self-calibration Gain)
+    #define DATA0     0x14
+    #define DATA1     0x15
+    #define DATA2     0x16
+    #define DATA3     0x17
+    #define DATA4     0x18
+    #define DATA5     0x19
 
-#define	MEASURE_1_SPS			0x80
-#define	MEASURE_2p5_SPS		0x81
-#define	MEASURE_5_SPS			0x82
-#define	MEASURE_10_SPS		0x83
-#define	MEASURE_15_SPS		0x84
-#define	MEASURE_30_SPS		0x85
-#define	MEASURE_60_SPS		0x86
-#define	MEASURE_120_SPS		0x87
+    // COMMAND BYTES  (MODE = 0, LINEF = 0)
+    //CTRL1 : Control Register 1
+    //This are all masked.  Need to read then 'AND' with register
+   //Calibration
+    #define	SELF_CALIB				0x3f  // [Bit7: 0, Bit6: 0, Bit5: x, Bit4: x, Bit3: x, Bit2: x, Bit1: x, Bit0: x]
+    #define	SLOC_CALIB				0x7f  // [Bit7: 0, Bit6: 1, Bit5: x, Bit4: x, Bit3: x, Bit2: x, Bit1: x, Bit0: x]
+    #define	SYSF_CALIB				0xbf  // [Bit7: 1, Bit6: 0, Bit5: x, Bit4: x, Bit3: x, Bit2: x, Bit1: x, Bit0: x]
+    #define	SELF_CALIB				0xff  // [Bit7: 1, Bit6: 1, Bit5: x, Bit4: x, Bit3: x, Bit2: x, Bit1: x, Bit0: x] DO NOT USE
+    //Power state
+    #define	POWER_NOP 				0xcf  // [Bit7: x, Bit6: x, Bit5: 0, Bit4: 0, Bit3: x, Bit2: x, Bit1: x, Bit0: x]
+    #define	POWER_SLEEP 		  0xdf  // [Bit7: x, Bit6: x, Bit5: 0, Bit4: 1, Bit3: x, Bit2: x, Bit1: x, Bit0: x]
+    #define	POWER_STANDBY 		0xef  // [Bit7: x, Bit6: x, Bit5: 1, Bit4: 0, Bit3: x, Bit2: x, Bit1: x, Bit0: x]
+    #define	POWER_RESET 			0xfF  // [Bit7: x, Bit6: x, Bit5: 1, Bit4: 1, Bit3: x, Bit2: x, Bit1: x, Bit0: x]
+    //Conversion type
+    #define CONN_POLAR_TYPE   0xF7  // [Bit7: x, Bit6: x, Bit5: x, Bit4: x, Bit3: 0, Bit2: x, Bit1: x, Bit0: x] 
+    //Format
+    #define DATA_FORMAT       0xfb  // [Bit7: x, Bit6: x, Bit5: x, Bit4: x, Bit3: x, Bit2: 0, Bit1: x, Bit0: x]
+    //Conversion scycle type: Single vs Continuous
+    #define CONV_SCYCLE       0xfd  // [Bit7: x, Bit6: x, Bit5: x, Bit4: x, Bit3: x, Bit2: x, Bit1: 0, Bit0: x]
+    /Conversion scycle type: Single vs Continuous
+    #define CONV_CONTSC       0xfe  // [Bit7: x, Bit6: x, Bit5: x, Bit4: x, Bit3: x, Bit2: x, Bit1: x, Bit0: 0]
 
-// MEASUREMENT MODE
-#define MODE_SINGLE				0x02
-#define MODE_CONTINUOUS		0x00
+    //***********************************************************************************************************
+    //CTRL2 : Control Register 2
+    //This are all masked.  
 
-//ADC WAIT TIMEOUT
-#define MAX_TIME_TO_WAIT 5  //this is in seconds
+    //Clock Mode
+     #define EXTCLK_MODE         0x00 // [Bit7: 0, Bit6: x, Bit5: x, Bit4: x, Bit3: x, Bit2: x, Bit1: x, Bit0: 0]
+    //External clock: 0x80
+    //Internal clock: 0x00
+    #define CCSEN 0x40
+    //CCSEN: 0x40 enables current source current sink on the analog inputs to detect sensor opens and shorts
+    #define LDOEN 0x20
+    //Set '1' to enable internal LDO
+    #define LPMODE      0x10
+    #define PGAEN       0x80
+    #define PGA_GAIN1   0x00
+    #define PGA_GAIN2   0x01
+    #define PGA_GAIN4   0x02
+    #define PGA_GAIN8   0x03
+    #define PGA_GAIN16  0x04
+    #define PGA_GAIN32  0x05
+    #define PGA_GAIN64  0x06
+    #define PGA_GAIN128 0x07
+    //***********************************************************************************************************
+    //CTRL3 : Control Register 3
+    //This are all masked. 
+    #define GPO_MODE  6
+    #define SYNC_MODE 5
+    #define CALREGSEL 4
+    #define NOSYSG    3
+    #define NOSYSO    2
+    #define NOSCG     1
+    #define NOSCO     0
+    //***********************************************************************************************************
+    //GPIO_CTRL: GPIO Control Register
+    #define GPIO_CTRL_REG 0x40
+    //  #define	SYS_OFFSET_CALIB	0xA0
+    //  #define	SYS_GAIN_CALIB		0xB0
+    #define	MEASURE_1_SPS			0x80
+    #define	MEASURE_2p5_SPS		0x81
+    #define	MEASURE_5_SPS			0x82
+    #define	MEASURE_10_SPS		0x83
+    #define	MEASURE_15_SPS		0x84
+    #define	MEASURE_30_SPS		0x85
+    #define	MEASURE_60_SPS		0x86
+    #define	MEASURE_120_SPS		0x87
+#endif
 
+#ifdef MAX11210
+  #define SELF_CALIB 0x00 //CTRL 1
+  // MEASUREMENT MODE
+  #define MODE_SINGLE				0x02
+  #define MODE_CONTINUOUS		0x00
+
+  //ADC WAIT TIMEOUT
+  #define MAX_TIME_TO_WAIT 5  //this is in seconds
+#endif
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 void SPI_init(void);
